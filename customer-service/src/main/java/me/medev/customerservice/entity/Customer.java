744ac5eaa35de1794c0933c459e371ea0458740f
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2025-11-29 15:03:32 +0100

    Add microservices architecture with config, customer, and eureka services

---
+++ customer-service/src/main/java/me/medev/customerservice/entity/Customer.java
@@ -0,0 +1,28 @@
+package me.medev.customerservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+
+    private String address;
+}
