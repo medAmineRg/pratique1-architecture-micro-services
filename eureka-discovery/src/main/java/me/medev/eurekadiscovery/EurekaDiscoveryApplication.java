cd79cd8d8e10f30b272bbfdfc37a9eb3759cb32b
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2025-11-29 18:21:36 +0100

    Update gateway service configuration: change active profile to 'docker', adjust service URIs, and add gateway-service.yml for routing

--- eureka-discovery/src/main/java/me/medev/eurekadiscovery/EurekaDiscoveryApplication.java
+++ eureka-discovery/src/main/java/me/medev/eurekadiscovery/EurekaDiscoveryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+
 public class EurekaDiscoveryApplication {
 
     public static void main(String[] args) {
