23255f8ec8a793039148eaeb3b425c66b5d55e75
Mohamed Amine Rguig <amine.mohamed.rg@gmail.com>
2025-11-30 22:43:19 +0100

    Add configuration for chatbot service with PostgreSQL datasource and JPA settings

--- init-db.sql
+++ init-db.sql
@@ -2,8 +2,10 @@
 CREATE SCHEMA IF NOT EXISTS customer_schema;
 CREATE SCHEMA IF NOT EXISTS inventory_schema;
 CREATE SCHEMA IF NOT EXISTS billing_schema;
+CREATE SCHEMA IF NOT EXISTS chatbot_schema;
 
 -- Grant permissions
 GRANT ALL PRIVILEGES ON SCHEMA customer_schema TO postgres;
 GRANT ALL PRIVILEGES ON SCHEMA inventory_schema TO postgres;
 GRANT ALL PRIVILEGES ON SCHEMA billing_schema TO postgres;
+GRANT ALL PRIVILEGES ON SCHEMA chatbot_schema TO postgres;
